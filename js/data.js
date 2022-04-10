b610ca8db1d67c6d6f94459bd43b3e65516e1f48
Konstantin Surikov <surikov@gmail.com>
2022-04-10 16:32:11 +0300

    module5-task1 Added modules

---
+++ js/data.js
@@ -0,0 +1,60 @@
+import {getRandomFromArray, getRandomInt, shuffle} from './utils.js';
+
+const MIN_COMMENTS = 0;
+const MAX_COMMENTS = 4;
+
+const NAMES = ['Аким', 'Ким', 'Николай', 'Степан', 'Тимур', 'Харитон'];
+
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const DESCRIPTIONS = [
+  'Если чётко сформулировать желание для Вселенной, то всё обязательно сбудется. Верьте в себя. Главное хотеть и мечтать..... / I\'ve bought some potatoes.',
+  'Как же круто тут кормят #food #foodgram #instafood #delicious #yummy',
+  '#fun #party #cool #young',
+  'Норм',
+  'Вот это тачка! #wow #car #carwow #drive',
+  'Тестим новую камеру! #camera #test #new #newcameratest #pic #photo #instaphoto',
+  'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......',
+  'Летний чил на югах. #тай #отдых #лето #чил #travel #travelgram #summergram #chill',
+  'Хорошо, когда в жизни есть #друзья, которые вместе со мной могут зайти в #барнарубинштейна и бахнуть #пивка',
+  'Затусили с друзьями на море #laptevsea #north #northeastpassage',
+  'Господи, это такая милота, я сейчас умру от нежности, у меня закшалил мимимиметр',
+  'Отдыхаем... #chill #relax #group #photo',
+];
+
+
+const createCommentMessage = (array) => {
+  const commentLength = getRandomInt(1, 2);
+  return shuffle(array).slice(0, commentLength).join(' ');
+};
+
+const createComment = (postId, commentId) => (
+  {
+    id: parseInt(postId.toString() + commentId.toString(), 10),
+    avatar: `img/avatar-${getRandomInt(1, 6)}`,
+    message: createCommentMessage(MESSAGES),
+    name: getRandomFromArray(NAMES)
+  });
+
+const getComments = (postId, limit) => new Array(limit)
+  .fill({})
+  .map((_, commentId) => createComment(postId, commentId));
+
+const createPost = (id) => ({
+  id: id,
+  url: `photos/${id}.jpg`,
+  description: getRandomFromArray(DESCRIPTIONS),
+  likes: getRandomInt(15, 200),
+  comments: getComments(id, getRandomInt(MIN_COMMENTS, MAX_COMMENTS))
+});
+
+const getPosts = (limit) => new Array(limit).fill({}).map((_, id) => createPost(id));
+
+export {getPosts};
---
+++ js/utils.js
@@ -0,0 +1,27 @@
+const checkLength = (string, length) => string.length <= length;
+
+const getRandomInt = (a, b) => {
+  const [from, to] = [
+    Math.ceil(Math.min((Math.abs(a)), (Math.abs(b)))),
+    Math.ceil(Math.max((Math.abs(a)), (Math.abs(b))))
+  ];
+  return Math.floor(Math.random() * (to - from + 1) + from);
+};
+
+const getRandomFromArray = (array) => array[getRandomInt(0, array.length - 1)];
+
+const shuffle = (array) => {
+  const result = array.slice();
+  for (let i = array.length - 1; i > 0; i--) {
+    const randomIndex = getRandomInt(0, i - 1);
+    [result[i], result[randomIndex]] = [result[randomIndex], result[i]];
+  }
+  return result;
+};
+
+export {
+  checkLength,
+  getRandomFromArray,
+  getRandomInt,
+  shuffle,
+};
